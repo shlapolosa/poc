e16a46bdd5fc29a4e101378be7298c4016e6675b
shlapolosa <socrates.hlapolosa@gmail.com>
2015-06-04 13:55:40 +0200

    added timeseries graph

--- DemandCaptureClient/ui/src/main/features/demand/controller/graphs.js
+++ DemandCaptureClient/ui/src/main/features/demand/controller/graphs.js
@@ -12,27 +12,21 @@
     app.controller('GraphController', function ($log, $scope, $socket) {
 
         $scope.data = [];
-        $scope.datapoints = [];
+        $scope.datapoints=[];
+        $scope.datacolumns=[{"id":"top-1","type":"line","name":"Top one","color":"black"},
+            {"id":"top-2","type":"spline","name":"Top two"}];
+
+        $scope.datax={"id":"x"};
 
 
-        var chart = c3.generate({
-            bindto: '#chart',
-            data: {
-                columns: [
-                    ['data1', 30, 200, 100, 400, 150, 250],
-                    ['data2', 50, 20, 10, 40, 15, 25]
-                ]
-            }
-        });
-
-        $socket.on('demand', function (data) {
+    $socket.on('demand', function (data) {
             var event = data;
             $scope.data.push(event);
-            var date = event.date;
+            var date = new Date(event.date);
             var v = event.demand.valueOrFailure;
             var t = event.demand.type;
             console.log({"x": date, "top-1": v, "top-2": t});
-            $scope.datapoints.push({"x": event.date, "top-1": event.valueOrFailure, "top-2": event.type});
+            $scope.datapoints.push({"x": date, "top-1": 1, "top-2": 10});
         });
 
 
@@ -41,4 +35,4 @@
         $scope.datax = {"id": "x"};
 
     });
-})(angular.module('demand.graph.controller', ['ngRoute', 'ngSocket']));
+})(angular.module('demand.graph.controller', ['ngRoute', 'ngSocket', 'gridshore.c3js.chart']));
