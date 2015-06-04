03973fb5f7f8b907ecba9146c3c724aacd5f97da
shlapolosa <socrates.hlapolosa@gmail.com>
2015-06-04 12:35:37 +0200

    fixed graphs not showing, issue was mostly browser caching

--- DemandCaptureClient/ui/src/main/features/demand/controller/graphs.js
+++ DemandCaptureClient/ui/src/main/features/demand/controller/graphs.js
@@ -14,16 +14,16 @@
         $scope.data = [];
         $scope.datapoints = [];
 
-        //
-        //var chart = c3.generate({
-        //    bindto: '#chart',
-        //    data: {
-        //        columns: [
-        //            ['data1', 30, 200, 100, 400, 150, 250],
-        //            ['data2', 50, 20, 10, 40, 15, 25]
-        //        ]
-        //    }
-        //});
+
+        var chart = c3.generate({
+            bindto: '#chart',
+            data: {
+                columns: [
+                    ['data1', 30, 200, 100, 400, 150, 250],
+                    ['data2', 50, 20, 10, 40, 15, 25]
+                ]
+            }
+        });
 
         $socket.on('demand', function (data) {
             var event = data;
