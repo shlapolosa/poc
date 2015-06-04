7bfff810cf88705d01f62d21ded491ef344d19c5
shlapolosa <socrates.hlapolosa@gmail.com>
2015-06-04 14:51:36 +0200

    made it point to demand/add by default

--- DemandCaptureClient/ui/src/main/features/demand/demand.js
+++ DemandCaptureClient/ui/src/main/features/demand/demand.js
@@ -1,7 +1,10 @@
 angular.module('demand',
     [
+        'ngRoute',
         'demand.add.controller',
         'demand.add.service',
         'demand.graph.controller'
     ]
-);
+).config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/demand/add'});
+    }]);
