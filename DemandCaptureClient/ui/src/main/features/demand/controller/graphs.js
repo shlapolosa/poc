e8c6e42130ab3f67dd7b40ae92c6c3577890ee35
shlapolosa <socrates.hlapolosa@gmail.com>
2015-06-04 14:16:29 +0200

    removed annoying message

--- DemandCaptureClient/ui/src/main/features/demand/controller/graphs.js
+++ DemandCaptureClient/ui/src/main/features/demand/controller/graphs.js
@@ -12,21 +12,27 @@
     app.controller('GraphController', function ($log, $scope, $socket) {
 
         $scope.data = [];
-        $scope.datapoints=[];
-        $scope.datacolumns=[{"id":"top-1","type":"line","name":"Top one","color":"black"},
-            {"id":"top-2","type":"spline","name":"Top two"}];
+        $scope.datapoints = [];
+        $scope.datacolumns = [{"id": "top-1", "type": "line", "name": "Top one", "color": "black"},
+            {"id": "top-2", "type": "spline", "name": "Top two"}];
 
-        $scope.datax={"id":"x"};
+        $scope.datax = {"id": "x"};
 
 
-    $socket.on('demand', function (data) {
-            var event = data;
-            $scope.data.push(event);
-            var date = new Date(event.date);
-            var v = event.demand.valueOrFailure;
-            var t = event.demand.type;
-            console.log({"x": date, "top-1": v, "top-2": t});
-            $scope.datapoints.push({"x": date, "top-1": 1, "top-2": 10});
+        $socket.on('demand', function (data) {
+
+            if(!data.message){
+                var event = data;
+                var date = new Date(event.date);
+                var v = event.demand.valueOrFailure;
+                var t = event.demand.type;
+                $scope.datapoints.push({
+                    "x": date, "top-1": Math.floor((Math.random() * 200) + 1)
+                    , "top-2": Math.floor((Math.random() * 100) + 1)
+                });
+            }else{
+                $scope.data.push(data.message);
+            }
         });
 
 
